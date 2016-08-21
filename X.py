298ac67a9d2c26e897efd0201a28b445e07492dc
Holger Ballweg <hb@uiae.de>
2016-08-21 10:22:34 +0100

    Add pyosc as submodule to make it easier to install
    - remove note about installing pyosc from readme
    - add trick to include submodule to X.py

--- X.py
+++ X.py
@@ -44,6 +44,11 @@ def calculate_position(eye, point,distance_offset=0):
 
 
 # osc
+import inspect
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"pyosc")))
+if cmd_subfolder not in sys.path:
+     sys.path.insert(0, cmd_subfolder)
+
 from OSC import OSCClient, OSCMessage
 
 client = OSCClient()
